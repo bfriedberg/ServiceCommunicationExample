9c2b3332249f441f367818d09bd93e1e439e9db5
bfriedberg <bfriedberg@sdgsystems.com>
2015-04-24 16:48:58 -0400

    adding simple example alongside larger one

---
+++ simpleserviceinteraction/src/main/java/com/sdgsystems/simpleserviceinteraction/MainActivity.java
@@ -0,0 +1,103 @@
+package com.sdgsystems.simpleserviceinteraction;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity implements View.OnClickListener{
+
+    private LinearLayout logMessages;
+
+    private BinderService.SynchronousServiceBinder mSyncServiceBinder;
+
+    private Button syncButton, syncTaskButton, clearButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        logMessages = (LinearLayout) findViewById(R.id.results);
+
+        syncButton = (Button) findViewById(R.id.btnSync);
+        syncTaskButton = (Button) findViewById(R.id.btnSyncTask);
+        clearButton = (Button) findViewById(R.id.btnClear);
+
+        syncButton.setOnClickListener(this);
+        syncTaskButton.setOnClickListener(this);
+        clearButton.setOnClickListener(this);
+
+
+        Intent syncServiceIntent = new Intent(this, BinderService.class);
+
+        this.bindService(syncServiceIntent, mSynchronousConnection, Context.BIND_AUTO_CREATE);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if(view == syncButton) {
+
+            logMessages.addView(getTextView("Calling synchronous service"));
+            int random = mSyncServiceBinder.getRandomNumber(5);
+            logMessages.addView(getTextView("Got synchronous random number: " + random));
+
+        } else if(view == syncTaskButton) {
+
+            logMessages.addView(getTextView("Calling synchronous service in an AsyncTask"));
+            CallSyncServiceTask task = new CallSyncServiceTask();
+            task.execute(5);
+
+        }  else if(view == clearButton) {
+            logMessages.removeAllViews();
+        }
+    }
+
+    private ServiceConnection mSynchronousConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+
+            logMessages.addView(getTextView("Bound to Synchronous Service"));
+            mSyncServiceBinder = (BinderService.SynchronousServiceBinder) service;
+
+        }
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+            logMessages.addView(getTextView("Unbound from Synchronous Service"));
+            mSyncServiceBinder = null;
+        }
+    };
+
+    private class CallSyncServiceTask extends AsyncTask<Integer, Void, Integer> {
+
+        @Override
+        protected Integer doInBackground(Integer... parms) {
+            return mSyncServiceBinder.getRandomNumber(parms[0]);
+        }
+
+        protected void onPostExecute(Integer randomNumber) {
+            logMessages.addView(getTextView("Received service response at end of asynctask: " + randomNumber));
+        }
+
+    }
+
+    private View getTextView(String s) {
+        TextView tv = new TextView(this);
+        tv.setText(s);
+        return tv;
+    }
+}
