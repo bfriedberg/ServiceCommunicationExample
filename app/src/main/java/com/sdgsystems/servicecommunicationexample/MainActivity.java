7a9d27c738e5dbbb116f26fe9fb0c58fbcdd7b55
bfriedberg <bfriedberg@sdgsystems.com>
2015-04-24 16:52:56 -0400

    removed synchronous service from main example

--- app/src/main/java/com/sdgsystems/servicecommunicationexample/MainActivity.java
+++ app/src/main/java/com/sdgsystems/servicecommunicationexample/MainActivity.java
@@ -29,10 +29,9 @@ public class MainActivity extends Activity implements View.OnClickListener{
 
     private LinearLayout logMessages;
 
-    private BinderService.SynchronousServiceBinder mSyncServiceBinder;
     private BinderServiceWithMessenger.AsynchronousServiceBinder mAsyncServiceBinder;
 
-    private Button asyncButton, syncButton, syncTaskButton, clearButton, heartbeatStart, heartbeatStop;
+    private Button asyncButton, clearButton, heartbeatStart, heartbeatStop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,43 +39,24 @@ public class MainActivity extends Activity implements View.OnClickListener{
         setContentView(R.layout.activity_main);
         logMessages = (LinearLayout) findViewById(R.id.results);
 
-        syncButton = (Button) findViewById(R.id.btnSync);
-        syncTaskButton = (Button) findViewById(R.id.btnSyncTask);
         asyncButton = (Button) findViewById(R.id.btnAsync);
         clearButton = (Button) findViewById(R.id.btnClear);
         heartbeatStart = (Button) findViewById(R.id.btnHeartbeatStart);
         heartbeatStop = (Button) findViewById(R.id.btnHeartbeatStop);
 
-        syncButton.setOnClickListener(this);
-        syncTaskButton.setOnClickListener(this);
         asyncButton.setOnClickListener(this);
         clearButton.setOnClickListener(this);
         heartbeatStart.setOnClickListener(this);
         heartbeatStop.setOnClickListener(this);
 
-
-        Intent syncServiceIntent = new Intent(this, BinderService.class);
         Intent asyncServiceintent = new Intent(this, BinderServiceWithMessenger.class);
-        this.bindService(syncServiceIntent, mSynchronousConnection, Context.BIND_AUTO_CREATE);
         this.bindService(asyncServiceintent, mAsynchronousConnection, Context.BIND_AUTO_CREATE);
     }
 
 
     @Override
     public void onClick(View view) {
-        if(view == syncButton) {
-
-            logMessages.addView(getTextView("Calling synchronous service"));
-            int random = mSyncServiceBinder.getRandomNumber(5);
-            logMessages.addView(getTextView("Got synchronous random number: " + random));
-
-        } else if(view == syncTaskButton) {
-
-            logMessages.addView(getTextView("Calling synchronous service in an AsyncTask"));
-            CallSyncServiceTask task = new CallSyncServiceTask();
-            task.execute(5);
-
-        } else if( view == asyncButton) {
+        if( view == asyncButton) {
 
             logMessages.addView(getTextView("Calling asynchronous service"));
             mAsyncServiceBinder.requestRandomNumberAsync(5);
@@ -94,22 +74,6 @@ public class MainActivity extends Activity implements View.OnClickListener{
             mAsyncServiceBinder.stopHeartBeatThread();
         }
     }
-
-    private ServiceConnection mSynchronousConnection = new ServiceConnection() {
-        @Override
-        public void onServiceConnected(ComponentName name, IBinder service) {
-
-            logMessages.addView(getTextView("Bound to Synchronous Service"));
-            mSyncServiceBinder = (BinderService.SynchronousServiceBinder) service;
-
-        }
-        @Override
-        public void onServiceDisconnected(ComponentName name) {
-
-            logMessages.addView(getTextView("Unbound from Synchronous Service"));
-            mSyncServiceBinder = null;
-        }
-    };
 
     private ServiceConnection mAsynchronousConnection = new ServiceConnection() {
         @Override
@@ -153,19 +117,6 @@ public class MainActivity extends Activity implements View.OnClickListener{
          }
      }
     });
-
-    private class CallSyncServiceTask extends AsyncTask<Integer, Void, Integer> {
-
-        @Override
-        protected Integer doInBackground(Integer... parms) {
-            return mSyncServiceBinder.getRandomNumber(parms[0]);
-        }
-
-        protected void onPostExecute(Integer randomNumber) {
-            logMessages.addView(getTextView("Received service response at end of asynctask: " + randomNumber));
-        }
-
-    }
 
     private View getTextView(String s) {
         TextView tv = new TextView(this);
